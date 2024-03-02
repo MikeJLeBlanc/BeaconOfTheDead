dd07228008089a1f8b5cddbc7261bd66fd1dcfc2
Mike LeBlanc <113053540+MikeJLeBlanc@users.noreply.github.com>
2024-03-01 20:58:09 -0400

    Add files via upload

---
+++ Scripts/LighthouseScript.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+var elapsedTime = 110.0
+var twoMinuteMarker = 120
+var thirtySecondTimer = 30
+var isEnemyMad = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$LightPivot.rotation += delta 
+	elapsedTime += delta
+	
+	'''
+	We need to check to see if the timers have elapsed for our enemy state.
+	Every 2 minutes enemy state changes into a rage state where spawn and speed of enemies double.
+	Visual changes include the lighthouse light turning read.
+	'''
+	if elapsedTime >= thirtySecondTimer && isEnemyMad == true:
+		isEnemyMad = false
+		changeEnemyState()
+		elapsedTime = 0.0
+	if elapsedTime >= twoMinuteMarker:
+		isEnemyMad = true
+		changeEnemyState()
+		elapsedTime = 0.0
+	
+
+func changeEnemyState():
+	if isEnemyMad:
+		$LightPivot/NormalLight.visible = false
+		$LightPivot/RedLight.visible = true
+		isEnemyMad = false
+	else:
+		$LightPivot/NormalLight.visible = true
+		$LightPivot/RedLight.visible = false
