ec12229b78c0e4b68d996ef0aa5be8fcd414f09b
Mike LeBlanc <mike_leblanc232@hotmail.com>
2024-04-20 13:55:35 -0300

    movement on player sprites

--- components/move_input_comoponent.gd
+++ components/move_input_comoponent.gd
@@ -1,36 +1,44 @@
 class_name MoveInputComponent
 extends Node
 
-var move_component: MoveComponent
-var move_speed: int
-var ship
-var ship_flames
+@export var move_component: MoveComponent
+@export var move_speed: int
+@export var player_sprite: AnimatedSprite2D
 
 
 func _ready():
-	move_component = $"../MoveComponent"
-	ship = $"../Anchor/Ship"
-	ship_flames = $"../Anchor/FlameSprite"
 	move_speed = 75
 
-
+# x axis horizontal movement
+# y axis vertical movement
 func _input(event: InputEvent) -> void:
-	var input_axis
-	if Input.is_action_pressed("move_left"):
-		input_axis = -1
-		_move_component(input_axis)
-		ship.play("bank_right")
-		ship_flames.play("bank_right")
-	elif Input.is_action_pressed("move_right"):
-		input_axis = 1
-		_move_component(input_axis)
-		ship.play("bank_left")
-		ship_flames.play("bank_left")
+	var input_axis_x = 0
+	var input_axis_y = 0
+	if Input.is_action_pressed("MoveLeft"):
+		input_axis_x = -1
+		input_axis_y = 0
+		player_sprite.flip_h = true
+		player_sprite.play("move")
+		_move_component(input_axis_x, input_axis_y)
+	elif Input.is_action_pressed("MoveRight"):
+		input_axis_x = 1
+		input_axis_y = 0
+		player_sprite.flip_h = false
+		player_sprite.play("move")
+		_move_component(input_axis_x, input_axis_y)
+	elif Input.is_action_pressed("MoveDown"):
+		input_axis_x = 0
+		input_axis_y = 1
+		player_sprite.play("move")
+		_move_component(input_axis_x, input_axis_y)
+	elif Input.is_action_pressed("MoveUp"):
+		input_axis_x = 0
+		input_axis_y = -1
+		player_sprite.play("move")
+		_move_component(input_axis_x, input_axis_y)
 	else:
-		input_axis = 0
-		_move_component(input_axis)
-		ship.play("level")
-		ship_flames.play("level")
+		player_sprite.play("idle")
+		_move_component(0, 0)
 
-func _move_component(input_axis) -> void:
-	move_component.velocity = Vector2(input_axis * move_speed, 0)
+func _move_component(input_axis_x, input_axis_y) -> void:
+	move_component.velocity = Vector2(input_axis_x * move_speed, input_axis_y * move_speed)
